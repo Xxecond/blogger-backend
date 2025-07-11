003254f5b89722575672eaee172b13d9e8e5ff42
xxecond <andrewsampadu9@gmail.com>
2025-07-11 11:36:10 +0000

    Project Changes5

--- express-basics/backend/config/db.js
+++ express-basics/backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const connectDB = async () => {q
+const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
